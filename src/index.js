ff479bb57b1284bd7aef463927c2bef3e14ab0e2
Aksy K <akshaykolhe04@gmail.com>
2024-01-05 00:03:47 +0530

    add utility functions and classes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env, PORT || 8000, () => {
+      console.log("Server is running at PORT: ", process.env.PORT);
+    });
+  })
+  .catch((err) => console.log("Mongodb connection failed!!", err));
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
