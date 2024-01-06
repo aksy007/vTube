1f2392c07f0c4fc3ca9b9d81f190a3d240d1b581
Aksy K <akshaykolhe04@gmail.com>
2024-01-07 00:19:16 +0530

    setup cloudinary and multer for filehandeling

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,7 +59,7 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordValid = async function (password) {
   return await bcrypt.compare(password, this.password);
-}; 
+};  
 
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
