813a4342e1b87accde3c231de52c769ee13eec6b
asky007 <akshaykolhe04@gmail.com>
2024-01-21 18:23:52 +0530

    Add todo's for future work

--- src/app.js
+++ src/app.js
@@ -20,8 +20,20 @@ app.use(cookieParser());
 
 // routes importing
 import userRouter from "./routes/user.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 // route declearation
 app.use("/api/v1/users", userRouter);
+app.use("/app/v1/likes", likeRouter);
+app.use("/app/v1/videos", videoRouter);
+app.use("/app/v1/comment", commentRouter);
+app.use("/app/v1/tweets", tweetRouter);
+app.use("/app/v1/playlist", playlistRouter);
+app.use("/app/v1/subscriptions", subscriptionRouter);
 
 export default app;
